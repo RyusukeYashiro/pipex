c39f8562239008c7c103b77319614006dbfdef9e
ryusukeyasiro <ryusukeyasiro@gmail.com>
2025-01-22 22:30:48 +0900

    error処理の実装

--- src/utils/errordeal.c
+++ src/utils/errordeal.c
@@ -6,31 +6,41 @@
 /*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 17:21:47 by ryyashir          #+#    #+#             */
-/*   Updated: 2025/01/21 19:14:13 by ryusukeyash      ###   ########.fr       */
+/*   Updated: 2025/01/22 22:27:39 by ryusukeyash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/libft.h"
 #include "../../include/pipex.h"
 
-void	ft_errorDeal(t_pipex pipex ,  const char *errormsg)
+void	ft_errorDeal(t_pipex *pipex ,  const char *errormsg)
 {
-	int		i;
-
-	i = 0;
-	//error出力の処理
-	if (ft_strncmp(errormsg, CMD_ERR, ft_strlen(CMD_ERR)) == 0)
-		ft_printf("%s: %s\n", pipex.cmd_split_in[0], errormsg);
+	ft_putstr_fd("bash: ", 2);
+	if (ft_strncmp(errormsg, FILE_ERR_P, ft_strlen(FILE_ERR_P)) == 0 || ft_strncmp(errormsg, FILE_ERR_N, ft_strlen(FILE_ERR_N)) == 0)
+		ft_printf("%s: %s\n", pipex->file_name, errormsg);
+	else if (ft_strncmp(errormsg, CMD_ERR, ft_strlen(CMD_ERR)) == 0)
+		ft_printf("%s: %s\n", pipex->cmd_split_in[0], errormsg);
 	else if (ft_strncmp(errormsg, DIR_ERR, ft_strlen(DIR_ERR)) == 0)
-		ft_printf("%s: %s\n", pipex.cmd_fullpath, errormsg);
-	else if (ft_strncmp(errormsg, FILE_ERR_P, ft_strlen(FILE_ERR_P)) == 0 || ft_strncmp(errormsg, FILE_ERR_N, ft_strlen(FILE_ERR_N)) == 0)
-		ft_printf("%s: %s\n", pipex.file_name, errormsg);
+		ft_printf("%s: %s\n", pipex->cmd_fullpath, errormsg);
 	else
 		ft_printf("%s\n", errormsg);
-	if(pipex.env_split)
-		ft_free_split(pipex.env_split);
-	if(pipex.cmd_fullpath)
-		free(pipex.cmd_fullpath);
-
+	if(pipex->env_split)
+	{
+		ft_free_split(pipex->env_split);
+		pipex->env_split = NULL;
+	}
+	if(pipex->cmd_fullpath)
+	{
+		free(pipex->cmd_fullpath);
+		pipex->cmd_fullpath = NULL;
+	}
 	exit(1);
+}
+
+void	ft_dup2_error(char **cmd , int fd)
+{
+	perror("Error");
+	ft_free_split(cmd);
+	close(fd);
+	exit(EXIT_FAILURE);
 }
